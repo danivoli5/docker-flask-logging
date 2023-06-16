e2b09791cf881e58a0de94d9636932cd1e2c5168
danivoli5 <57321488+danivoli5@users.noreply.github.com>
2023-06-16 14:29:18 +0300

    nginx-stickiness

--- scale_script.sh
+++ scale_script.sh
@@ -2,4 +2,20 @@
 
 read -p "Enter the desired scale value: " SCALE_VALUE
 
+# Update NGINX configuration based on the scale value
+if [[ $SCALE_VALUE -ge 1 ]]; then
+  # Generate the upstream backend server list
+  server_list=""
+  for ((i = 1; i <= $SCALE_VALUE; i++)); do
+    server_list+="server app-test-app-$i:5000;\n"
+  done
+
+  # Insert the server_list into the NGINX configuration file after the upstream backend block starts
+  sed -i "/upstream backend {/a $server_list" nginx/nginx.conf
+else
+  echo "Invalid scale value. Exiting..."
+  exit 1
+fi
+
+# Start the containers with the updated configuration
 docker-compose up --scale app=$SCALE_VALUE
